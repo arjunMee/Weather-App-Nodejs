82fbf4d0f624e776a66d7dbf2d70c35cb4cd6bcd
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-06-06 14:02:48 +0530

    removed few files

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,8 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
-  fetch('http://localhost:3000/weather?address=' + search.value).then((res) => {
+  // localhost:3000
+  http: fetch('/weather?address=' + search.value).then((res) => {
     res.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
